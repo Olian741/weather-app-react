319fb3440cdc81996e6265e47847314c45396246
Olian741 <158222992+Olian741@users.noreply.github.com>
2024-06-06 15:59:41 +0200

    created the weather.css file

--- src/App.js
+++ src/App.js
@@ -3,10 +3,18 @@ import './App.css';
 
 
 export default function App() {
-  return (
-    <div>
-      <h2 className="weather-app-app">Weather App</h2>
-    </div>
-  );
+  return(
+  <div className="weather">
+    <div className="container">
+    <footer>
+This project was coded by {" "}
+      <a href="https://github.com/Olian741">Olian Mkhawana</a>{" "}
+      and is {" "} 
+      <a href="https://github.com/weather-react-app"> open-soruced on GitHub </a>
+     and is hosted on <a href="https://netlify.com/weather-react-app"></a>
+    </footer>
+  </div>
+  </div>
+);
 }
        
--- src/Weather.js
+++ src/Weather.js
@@ -1,19 +1,10 @@
 import React from "react";
 
 
+
 export default function Weather(){
 return(
-  <div className="weather">
-    <div className="container">
-    <footer>
-      This project was coded by {" "}
-      <a href="https://github.com/Olian741">Olian Mkhawana</a>{" "}
-      and is {" "} 
-      <a href="https://github.com/weather-react-app"> open-soruced on GitHub </a>
-      {" "} and is hosted on <a href="https://netlify.com/weather-react-app"></a>
-    </footer>
-  </div>
-  </div>
+  <div className="Weather">Hello from Weather</div>
 )
 }
   
