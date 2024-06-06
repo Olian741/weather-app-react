b5c179160a159c9ae0f6f0e7f06bb6c836bc7026
Olian741 <158222992+Olian741@users.noreply.github.com>
2024-06-06 18:52:46 +0200

    Added the html and basic styling

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React from "react";
 import './App.css';
+import Weather from "./Weather";
 
 
 export default function App() {
   return(
   <div className="weather">
     <div className="container">
+      <Weather />
     <footer>
 This project was coded by {" "}
-      <a href="https://github.com/Olian741">Olian Mkhawana</a>{" "}
+      <a href="https://github.com/Olian741" target="_blank" rel="noreferrer">Olian Mkhawana</a>{" "}
       and is {" "} 
-      <a href="https://github.com/weather-react-app"> open-soruced on GitHub </a>
-     and is hosted on <a href="https://netlify.com/weather-react-app"></a>
+      <a href="https://github.com/weather-react-app" target="_blank" rel="noreferrer"> open-soruced on GitHub </a>
+     and is hosted on 
+     <a href="https://netlify.com/weather-react-app" target= "_blank" rel="noreferrer"
+     > netlify</a>
     </footer>
   </div>
   </div>
