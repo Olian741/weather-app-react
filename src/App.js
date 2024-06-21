2dbd0d4e8d0ae4c3f5c44b60035234f40355e615
Olian741 <158222992+Olian741@users.noreply.github.com>
2024-06-21 14:07:23 +0200

    Added the forecast api

--- src/App.js
+++ src/App.js
@@ -4,21 +4,40 @@ import Weather from "./Weather";
 
 
 export default function App() {
-  return(
-  <div className="weather">
-    <div className="container">
-      <Weather defaultCity= "New York"/>
-    <footer>
-This project was coded by {" "}
-      <a href="https://github.com/Olian741" target="_blank" rel="noreferrer">Olian Mkhawana</a>{" "}
-      and is {" "} 
-      <a href="https://github.com/weather-react-app" target="_blank" rel="noreferrer"> open-sourced on GitHub </a>
-     and is hosted on 
-     <a href="https://netlify.com/weather-react-app" target= "_blank" rel="noreferrer"
-     > netlify</a>
-    </footer>
-  </div>
-  </div>
-);
+  return (
+    <div className="weather">
+      <div className="container">
+        <Weather defaultCity="New York" />
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://github.com/Olian741"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Olian Mkhawana
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/Olian741/weather-app-react"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            open-sourced on GitHub{" "}
+          </a>
+          and is hosted on
+          <a
+            href="https://tranquil-kulfi-baf8fa.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            netlify
+          </a>
+        </footer>
+      </div>
+    </div>
+  );
 }
        
